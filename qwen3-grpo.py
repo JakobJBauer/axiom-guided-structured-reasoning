f3e324dfdcd0e85159454e2f93c666dd36629f02
Jakob Johannes Bauer <bauerjakobjohannes@gmail.com>
2025-12-05 16:18:46 +0000

    WIP

--- qwen3-grpo.py
+++ qwen3-grpo.py
@@ -7,13 +7,17 @@ def reward_num_unique_letters(completions, **kwargs):
     completions_contents = [completion[0]["content"] for completion in completions]
     return [float(len(set(content))) for content in completions_contents]
 
-training_args = GRPOConfig(output_dir="Qwen2-Instruct-GRPO")
+training_args = GRPOConfig(
+    output_dir="Qwen2-Instruct-GRPO"
+    report_to="wandb",
+    run_name="qwen2-grpo-unique-letters",
+)
+
 trainer = GRPOTrainer(
     model="Qwen/Qwen2-0.5B-Instruct",
     reward_funcs=reward_num_unique_letters,
     args=training_args,
-    train_dataset=dataset,
-    report_to="wandb",
-    run_name="qwen2-grpo-unique-letters",
+    train_dataset=dataset
 )
+
 trainer.train()
