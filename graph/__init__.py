022301d4cd7d2f8e879593dda553b4b8e0b31f7d
Jakob Johannes Bauer <bauerjakobjohannes@gmail.com>
2026-01-24 09:48:49 +0000

    better structure

---
+++ graph/__init__.py
@@ -0,0 +1,4 @@
+from .graph import Graph, Node, Edge
+from .formulas import Formula, Not, And, Xor, Or, Equal, In
+
+__all__ = ['Graph', 'Node', 'Edge', 'Formula', 'Not', 'And', 'Xor', 'Or', 'Equal', 'In']
